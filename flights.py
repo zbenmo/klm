fddbeef9c20a4c456bfe75fca038a0117540c725
Oren <zbenmo@gmail.com>
2022-06-28 22:13:53 +0200

    this looks okay

--- flights.py
+++ flights.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Generator
 from doctest import testmod
+import heapq
+from collections import defaultdict
 
 
 @dataclass
@@ -45,38 +47,39 @@ class FlightsSchedule:
     and assumes not a very long itenerary. 
     """
     def __init__(self):
-        self.bookings = []
+        self.bookings = [] # it will be a heap with pairs (departure, the actual booking)
+        self.airport_sequence_dict = defaultdict(list)
 
     def add_booking(self, booking:Booking) -> FlightsSchedule:
         "Adds a single booking to the collection."
         "This call is chainable."
 
-        assert f"received itenerary with length {len(booking.itenerary)}", len(booking.itenerary) < 10 
-        assert f"booking length is already {len(self.bookings)}", len(self.bookings) < 1000
-        self.bookings.append(booking)
+        # assumption it shouldn't happen. Iteneraries are not longer than 9.
+        assert f"received itenerary with length {len(booking.itenerary)}", len(booking.itenerary) < 10
+
+        heapq.heappush(self.bookings, (booking.departure, booking))
+        self.register_sequential_airport_couples(booking)
+
         return self
 
-        # TODO: if turns out to be used when a bigger number of bookings, prepare and maintain a few data structures,
-        # and keep those updated on each insertion, as an idea (or cache the results).
-        # For example, keep a dict (defaultdict(list)) from each possible sequence pair of airports -
-        # to a list of bookings for which this pair is found as a sequence:
-        # for booking in self.booking:
-        #   for idx in range(len(booking) - 2 + 1)):
-        #       d[(booking[idx], booking[idx + 1])].append(booking)    
-        # or (in addition) sort the bookings by departure date
-        # (do that with some caching mechanism that forces it to sort after each addition or at least before next query),
-        # and do a binary search when asked based on departure.   
+    def register_sequential_airport_couples(self, booking:Booking):
+        for airport1, airport2 in zip(booking.itenerary[:-1], booking.itenerary[1:]):
+            self.airport_sequence_dict[(airport1, airport2)].append(booking)    
 
     def select_bookings_by_latest_departure_time(self, departs_before:datetime) -> Generator[FlightsSchedule, None, None]:
         "Assumes the departs_before as UTC."
 
-        yield from (booking for booking in self.bookings if booking.departure < departs_before)
+        for departure, booking in self.bookings:
+            if departure < departs_before:
+                yield booking
+            else:
+                return
 
     def select_bookings_by_sequential_airports_couple(self, airport1:str, airport2:str) -> Generator[FlightsSchedule, None, None]:
         "Assumes airport1 and airport2 are IATA codes."
 
-        sequence_to_search = [airport1, airport2]
-        yield from (booking for booking in self.bookings if sublist_in_list(sequence_to_search, booking.itenerary))
+        yield from self.airport_sequence_dict[(airport1, airport2)]
+
 
 def date_from_string(input_str: str) -> datetime:
     "helper function for below test (main)"
